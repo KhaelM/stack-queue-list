6b73711c9d61448e0c24478b16bd9781653067bb
Michael Randria <mikerandria@gmail.com>
2019-05-14 23:33:59 +0300

    Queue Released

--- Queue.hpp
+++ Queue.hpp
@@ -5,18 +5,61 @@ class Queue
 {
 private:
     T* array;
+    int firstElementIndex = -1;
+    int lastElementIndex = -1;
+    int maxSize;
+    const int DEFAULT_SIZE = 100;
+
 public:
     Queue();
+    Queue(int initialSize);
     ~Queue();
+    bool isEmpty();
+    void enqueue(T); // Enfiler
+    T dequeue(); // Defiler
 };
 
 template<class T>
-Queue<T>::Queue(/* args */)
+Queue<T>::Queue()
 {
+    this->array = new T[this->DEFAULT_SIZE];
+}
+
+template<class T>
+Queue<T>::Queue(int initialSize)
+{
+    this->array = new T[initialSize];
+    this->maxSize = initialSize;
 }
 
 template<class T>
 Queue<T>::~Queue()
 {
+    delete this->array;
 }
 
+template<class T>
+bool Queue<T>::isEmpty()
+{
+    return this->lastElementIndex <= this->firstElementIndex;
+}
+
+template<class T>
+void Queue<T>::enqueue(T element)
+{
+    if(this->size >= this->maxSize - 1) {
+        throw new std::runtime_error("Impossible de d'enfiler: Le tableau est déjà plein.");
+    }
+
+    this->array[++this->lastElementIndex] = element;
+}
+
+template<class T>
+T Queue<T>::dequeue()
+{
+    if(this->isEmpty()) {
+        throw std::runtime_error("Impossible de defiler, le tableau est vide.");
+    }
+
+    return this->array[this->firstElementIndex++];
+}
